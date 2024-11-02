48de80b82a0f12243608b6ac598ed92cde75b816
Justin Gmys <jrgmys@gmail.com>
2024-11-02 14:58:50 -0400

    Set up database connection

---
+++ api/db.js
@@ -0,0 +1,12 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
