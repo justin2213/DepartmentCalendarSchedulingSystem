1385dffe276237475625ae9a116a640d9c59c5f0
Justin Gmys <jrgmys@gmail.com>
2024-11-02 14:45:56 -0400

    Installed dependencies and set up supabase client to use Microsoft Auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionContextProvider supabaseClient={supabase}>
+          <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
